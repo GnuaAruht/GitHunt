5db13705389e73cb824e35e1639c59beeaa87cac
thuraaung-ppt <thuraaung.ppt@gmail.com>
2020-04-26 09:10:57 +0630

    update ui of layout repo item and add some resources

--- app/src/main/java/com/thuraaung/githunt/utils/Constants.kt
+++ app/src/main/java/com/thuraaung/githunt/utils/Constants.kt
@@ -1,6 +1,6 @@
 package com.thuraaung.githunt.utils
 
-
+const val APP_NAME = "Githunt_App"
 
 const val APP_PREF = "APP_PREF"
 const val LANGUAGE = "language_key"
--- app/src/main/java/com/thuraaung/githunt/utils/Extensions.kt
+++ app/src/main/java/com/thuraaung/githunt/utils/Extensions.kt
@@ -1,19 +1,24 @@
 package com.thuraaung.githunt.utils
 
 import android.content.Context
+import android.graphics.Color
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.thuraaung.githunt.R
+import java.lang.IllegalArgumentException
 
 
 fun ImageView.loadImage(url : String) {
     Glide.with(context)
         .load(url)
-        .placeholder(R.drawable.github)
+        .placeholder(R.drawable.drawable_round_holder)
         .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
         .apply(RequestOptions.circleCropTransform())
         .into(this)
@@ -50,4 +55,15 @@ fun Context.savePreference(key : String, value : String) : Boolean {
 fun Context.getPreferenceString(key : String,defaultValue : String) : String {
     return getSharedPreferences(APP_PREF,Context.MODE_PRIVATE)
         .getString(key,null) ?: defaultValue
+}
+
+fun TextView.setDrawableBackgroundColor(color: String) {
+    compoundDrawables[0]?.let {
+        val wrappedDrawable = DrawableCompat.wrap(it)
+        try {
+            DrawableCompat.setTint(wrappedDrawable, Color.parseColor(color))
+        } catch (ignore: IllegalArgumentException) {
+            Log.e(APP_NAME,"Cannot set background color language")
+        }
+    }
 }
